f9601a898bf918152bb1a9100b0813fc6ec8be53
Amir Shaikh <149932327+Amyshk786@users.noreply.github.com>
2024-10-19 21:47:41 +0530

    Update server.js

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -5,7 +5,9 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5050"
+}));
 app.use(express.json());
 app.use("/record", records);
 
